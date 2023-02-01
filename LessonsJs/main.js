38dd0b58902eba0730bdc520622accc64c667b9d
Roman5658 <ozenenesis56@gmail.com>
2023-02-01 17:53:15 +0100

    Js Homeworks

---
+++ LessonsJs/main.js
@@ -0,0 +1,71 @@
+//ЗАДАНИЕ 1
+
+let strText = 'hello';
+console.log(strText)
+
+strText = 'owu';
+console.log(strText);
+
+strText = 'com';
+console.log(strText);
+
+strText = 'ua';
+console.log(strText);
+
+let num = 1;
+console.log(num)
+
+num = 10;
+console.log(num)
+
+num = -999;
+console.log(num)
+
+num = 123;
+console.log(num)
+
+const PI = 3.14;
+console.log(PI)
+
+const E = 2.7;
+console.log(E)
+
+num = 16;
+console.log(num)
+
+strText = 'true';
+console.log(strText)
+
+strText = 'false';
+console.log(strText)
+
+
+
+
+//ЗАДАНИЕ 2
+
+let firstName = 'Roman';
+let middleName = 'Nikolayevich';
+let lastName = 'Pysanyi';
+
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+
+
+//ЗАДАНИЕ 3
+
+let a = 100;
+console.log(typeof a)
+
+let b = '100';
+console.log(typeof b)
+
+let c = true;
+console.log(typeof c)
+
+
+
+//ЗАДАНИЕ 4
+
+let age = prompt(`Роман Миколайович 30`);
+console.log(age);
